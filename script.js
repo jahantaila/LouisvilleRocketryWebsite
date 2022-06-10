9a15fd8b55027f09af0822bb5b195892f2fb1aed
jahantaila <tailajahan@gmail.com>
2022-06-10 11:41:24 -0400

    almost done with home screen, working on responsivness and mobile layout, also added projects.html

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
 const hamburger = document.getElementById("hamburger");
 const mobNav = document.getElementById("mobile-nav");
 const cross = document.getElementById("cross");
-
+function scrollEvent() {
+    window.scrollTo(0, 0);
+}
 hamburger.addEventListener("click", () => {
     mobNav.classList.toggle("show");
+    window.scrollTo(0, 0);
 });
 
 cross.addEventListener("click", () => {
     mobNav.classList.toggle("show");
+    // window.removeEventListener("scroll", scrollEvent);
 });
 
 window.addEventListener("scroll", () => {
