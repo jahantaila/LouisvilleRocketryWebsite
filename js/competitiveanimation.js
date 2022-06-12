59a452b8a18c2a27f347bdf0dd97839e8fbbbf50
jahantaila <tailajahan@gmail.com>
2022-06-12 14:17:17 -0400

    home screen details

---
+++ js/competitiveanimation.js
@@ -0,0 +1,44 @@
+
+
+// window.addEventListener('scroll', function() {
+	var element = document.querySelector('#about-us');
+	var position = element.getBoundingClientRect();
+
+	// checking whether fully visible
+	if(position.top >= 0 && position.bottom <= window.innerHeight) {
+		new TypeIt("#competetivetext", { 
+      lifeLike: true, 
+      speed: 0 
+    })
+    .type("C")
+    .pause(300)
+    .type("o")
+    .pause(300)
+    .type("m")
+    .pause(300)
+    .type("p")
+    .pause(300)
+    .type("e")
+    .pause(300)
+    .type("t")
+    .pause(300)
+    .type("i")
+    .pause(300)
+    .type("t")
+    .pause(300)
+    .type("i")
+    .pause(300)
+    .type("v")
+    .pause(300)
+    .type("e")
+    .go();
+	}
+
+	
+});
+
+
+
+
+
+
