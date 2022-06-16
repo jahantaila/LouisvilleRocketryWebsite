fe2180a7e4fbc8b4d17df50b0d1f14e595dda19e
jahantaila <tailajahan@gmail.com>
2022-06-16 11:05:00 -0400

    FINISHED HOME PAGE

--- js/competitiveanimation.js
+++ js/competitiveanimation.js
@@ -1,45 +1,6 @@
-// NEED TO FIX - WANT TYPING ANIMATION TO START WHEN USER SCROLLS TO THE ABOUT-US SECTION
-// __________________________________________________________________________________________
-// window.addEventListener('scroll', function() {
-  /*
-	var element = document.querySelector('#about-us');
-	var position = element.getBoundingClientRect();
-
-	// checking whether fully visible
-	if(position.top >= 5 && position.bottom <= window.innerHeight) {
-		new TypeIt("#competetivetext", { 
-      lifeLike: true, 
-      speed: 0 
-    })
-    .type("C")
-    .pause(300)
-    .type("o")
-    .pause(300)
-    .type("m")
-    .pause(300)
-    .type("p")
-    .pause(300)
-    .type("e")
-    .pause(300)
-    .type("t")
-    .pause(300)
-    .type("i")
-    .pause(300)
-    .type("t")
-    .pause(300)
-    .type("i")
-    .pause(300)
-    .type("v")
-    .pause(300)
-    .type("e")
-    .go();
-	}
-
-	
-});
-
-
-
-*/
-
-
+new TypeIt("#competetivetext", {
+  lifeLike: true,
+  waitUntilVisible: true,
+})
+  .type("competitive")
+  .go();
